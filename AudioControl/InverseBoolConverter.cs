a14396811698bddbc28ad1ea5952b171b86df4a6
Leander Wollersberger <leander.wollersberger@gmx.at>
2025-05-22 13:26:25 +0200

    Fix warnings

--- AudioControl/InverseBoolConverter.cs
+++ AudioControl/InverseBoolConverter.cs
@@ -9,10 +9,18 @@ namespace AudioControl
 {
     public class InverseBoolConverter : IValueConverter
     {
-        public object Convert(object value, Type targetType, object parameter, CultureInfo culture) =>
-            value is bool b ? !b : true;
+        public object? Convert(object? value, Type targetType, object? parameter, CultureInfo culture)
+        {
+            if (value is bool boolValue)
+                return !boolValue;
+            return false;
+        }
 
-        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture) =>
-            value is bool b ? !b : false;
+        public object? ConvertBack(object? value, Type targetType, object? parameter, CultureInfo culture)
+        {
+            if (value is bool boolValue)
+                return !boolValue;
+            return false;
+        }
     }
 }
