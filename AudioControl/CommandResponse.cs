9ffc204cd6fd48387eede8419b6663f9e3ed28ca
Leander Wollersberger <leander.wollersberger@gmx.at>
2025-05-22 18:49:41 +0200

    JsonPropertyNames definieren um mit Rust Code übereinzustimmen

--- AudioControl/CommandResponse.cs
+++ AudioControl/CommandResponse.cs
@@ -2,17 +2,24 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Text.Json.Serialization;
 using System.Threading.Tasks;
 
 namespace AudioControl
 {
     public class CommandResponse
     {
+        [JsonPropertyName("resp")]
         public string Resp { get; set; } = string.Empty;
 
         // Optional depending on Resp
+        [JsonPropertyName("message")]
         public string? Message { get; set; }
+        
+        [JsonPropertyName("level")]
         public int? Level { get; set; }
+
+        [JsonPropertyName("muted")]
         public bool? Muted { get; set; }
     }
 }
